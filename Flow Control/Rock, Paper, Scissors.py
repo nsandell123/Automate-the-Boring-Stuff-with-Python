f6896fa55a0ad901919fff4e8f962ebdc3f17969
nsandell123 <sandellfamily2013@gmail.com>
2020-04-25 15:05:09 -0400

    Started Rock Paper Scissors Game

---
+++ Flow Control/Rock, Paper, Scissors.py
@@ -0,0 +1,21 @@
+#This is a program to play rock paper scissors
+
+#Use a control structure to play as many times as the user desire
+userInput = ""
+playerIndex = ["PRSP"]
+computerToken = ""
+while(userInput != "no"):
+    #Ask user for their token
+    userToken = input("Please enter R for rock, S for scissors, and P for paper:  ")
+    while(not(isinstance(userToken,str)) or userToken != "R" or userToken != "S" or userToken != "P"):
+        userToken = print("Please enter R for rock, S for scissors, and P for paper: ")
+    
+    #Generate a random Token 
+    
+    #Compare the two 
+
+    #return who won
+
+    #ask the user if they want to play again
+
+    #keep a counter to store number of losses and wins
