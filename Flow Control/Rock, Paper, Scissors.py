c9457e84b696ad02be8e039cf14b7d2d3fb5b66b
nsandell123 <sandellfamily2013@gmail.com>
2020-04-25 19:50:18 -0400

    I have finally finished this project

--- Flow Control/Rock, Paper, Scissors.py
+++ Flow Control/Rock, Paper, Scissors.py
@@ -1,22 +1,47 @@
-#This is a program to play rock paper scissors
+# This is a program to play rock paper scissors
+import random
+#This variable determines whether to keep playing the game. 
+userInput = "default"
+#This variable holds the value of R, P, or S for the user
+userToken = "default"
+#This string is used to determine the winner of the game
+playerIndex = "PRSP"
+#This list stores one string that the computer randomly chooses
+computerToken = []
+#this is the string inside the list pulled out
+updatedComputerToken = ""
+#This counts how many times the user has played
+counter = 0
+userInput = input("Do you want to play a game of rock paper scissors? ")
 
-#Use a control structure to play as many times as the user desire
-userInput = ""
-playerIndex = ["PRSP"]
-computerToken = ""
-while(userInput != "no"):
-    #Ask user for their token
-    userToken = input("Please enter R for rock, S for scissors, and P for paper:  ")
-    while(not(isinstance(userToken,str)) or userToken != "R" or userToken != "S" or userToken != "P"):
-        userToken = print("Please enter R for rock, S for scissors, and P for paper: ")
-    
-    #Generate a random Token 
-    print("Testing Testing Testing")
-    #Compare the two 
-    print("testingp2")
+while userInput == "yes":
+    while any([not(isinstance(userToken, str)), userToken not in ("R", "S", "P")]):
+        userToken = input("Please enter R for rock, S for scissors, and P for paper: ")
+        counter+=1
+    # after the user has entered the right one, make sure you get computer input
+    computerToken = random.choices(playerIndex, [0.165, 0.33, 0.33, 0.165], k=1)
+    updatedComputerToken = updatedComputerToken.join(computerToken)
+    # return who won
+    location = playerIndex.find(userToken)
+    if updatedComputerToken == playerIndex[location + 1]:
+        print("The computer has entered: " + updatedComputerToken)
+        print("Congratulations, you won")
+        userToken = "reset"
+        userInput = input("Do you want to play again? ")
+        if(userInput != "yes"):
+            print("You have played " + str(counter) + " time(s)")
+    elif userToken == updatedComputerToken:
+        print("The computer has entered: " + updatedComputerToken)
+        print("It is a tie")
+        userToken = "reset"
+        userInput = input("Do you want to play again? ")
+        if(userInput != "yes"):
+            print("You have played " + str(counter) + " time(s)")
+    else:
+        print("The computer has entered: " + updatedComputerToken)
+        print("You have lost")
+        userToken = "reset"
+        userInput = input("Do you want to play again? ")
+        if(userInput != "yes"):
+            print("You have played " + str(counter) + " time(s)")
 
-    #return who won
-
-    #ask the user if they want to play again
-
-    #keep a counter to store number of losses and wins
